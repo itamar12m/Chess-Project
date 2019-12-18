e4e17113c46416d62155be0d767b69547821de43
Itamar Malachi <itamar12malachi@gmail.com>
2019-12-18 18:29:35 +0200

    started write piece

---
+++ chessProject/Piece.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+
+using std::string;
+
+class Board;
+
+class Piece
+{
+public:
+	Piece(bool color, Board* board);
+	int move(string toMove);
+	~Piece();
+
+protected:
+	//metod:
+	int checkVallid(string toMove);
+
+	//fildes:
+	bool _color;
+	Board* _board;
+};
+
+Piece::Piece(bool color, Board* board) : _color(color), _board(board)
+{
+}
+
+Piece::~Piece()
+{
+}
+
