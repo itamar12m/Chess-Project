2c6338e6febba5eabfcf1a5ae34133838e6829ca
Itamar Malachi <itamar12malachi@gmail.com>
2019-12-20 16:01:58 +0200

    start check king

--- chessProject/King.cpp
+++ chessProject/King.cpp
@@ -6,9 +6,10 @@ King::King(bool color, Board* board) :Piece(color, board)
 
 int King::move(string indexes)
 {
-	/*if ()
+	if (indexes[2] + 1 == indexes[0] || indexes[2] - 1 == indexes[0] || indexes[2] == indexes[0] ||
+		indexes[3] + 1 == indexes[1] || indexes[3] - 1 == indexes[1] || indexes[3] == indexes[1])
 	{
-
-	}*/
+		return INVALID_PIECE_MOVE;
+	}
 	return 0;
 }
